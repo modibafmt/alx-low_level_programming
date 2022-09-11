5fc48cc3af4103607b9f9b83d90f2511592551ab
modibafmt <111021668+modibafmt@users.noreply.github.com>
2022-09-12 01:30:56 +0200

    Add files via upload

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		} else
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
