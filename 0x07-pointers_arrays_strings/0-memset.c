cde419db7ea27833808898895c68537bd645c5c0
modibafmt <111021668+modibafmt@users.noreply.github.com>
2022-09-27 11:52:16 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,21 @@
-/* memset
-   This implementation is in the public domain.  */
+#include "holberton.h"
 
-/*
-
-@deftypefn Supplemental void* memset (void *@var{s}, int @var{c}, size_t @var{count})
-
-Sets the first @var{count} bytes of @var{s} to the constant byte
-@var{c}, returning a pointer to @var{s}.
-
-@end deftypefn
-
-*/
-
-#include <ansidecl.h>
-#include <stddef.h>
-
-PTR
-memset (PTR dest, register int val, register size_t len)
+/**
+  * _memset - Fill memory with a constant byte
+  * @s: memory area to fill
+  * @b: constant byte to fill
+  * @n: bytes of memory area to fill
+  *
+  * Return: the memory area filled
+  */
+char *_memset(char *s, char b, unsigned int n)
 {
-  register unsigned char *ptr = (unsigned char*)dest;
-  while (len-- > 0)
-    *ptr++ = val;
-  return dest;
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
 }
